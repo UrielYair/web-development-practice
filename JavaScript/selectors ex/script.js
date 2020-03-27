1ab9f7f1cb049972a424af0195f8eacd542ffd23
Uriel Yair <uriel91@gmail.com>
2020-03-27 03:06:44 +0300

    Add basic practice of DOM manipulation.

--- JavaScript/selectors ex/script.js
+++ JavaScript/selectors ex/script.js
@@ -14,4 +14,27 @@ alert("content of #last before editing is: [" + last.textContent + "]");
 last.style.border = "5px solid black";
 
 // changing theme to dark:
-var pageBody = document.body.classList.toggle("darkTheme");
+var pageBody = document.body;
+pageBody.classList.toggle("darkTheme");
+
+// changing content of last:
+last.textContent = "It's not the end, only a beginning";
+
+// getting all the text from body:
+var pageText = document.body.textContent;
+console.log(pageText);
+
+var a = document.querySelector('a');
+alert("URL of the link is: " + a.getAttribute("href"));
+a.setAttribute("href", "http://www.UrielYair.com");
+alert("URL of the link after change is: " + a.getAttribute("href"));
+a.textContent = "link to www.UrielYair.com";
+
+var btn = document.querySelector("button");
+btn.addEventListener("click", function() {
+    pageBody.classList.toggle("darkTheme");
+    document.querySelector("button+p").textContent = "button was clicked!";    
+});
+
+
+
