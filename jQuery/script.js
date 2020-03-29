c646de2dc0217a7117c7c7339b226b0f840eec95
Uriel Yair <uriel91@gmail.com>
2020-03-29 08:53:51 +0300

    Add small jQuery practice ex.

---
+++ jQuery/script.js
@@ -0,0 +1,8 @@
+$("div").css("background","purple");
+$(".highlight").css("width","200px");
+$("#third").css({
+    "border-color" : "orange",
+    "border-style" : "solid",
+    "border-width" : "5px"
+});
+$("div").first().css("color","pink");
