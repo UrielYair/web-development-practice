30cb454a7ed8d26134ed90a5ed135d706c811029
Uriel Yair <uriel91@gmail.com>
2020-04-17 17:29:02 +0300

    Change PORT in RESTful BlogApp.

--- RESTful_BlogApp/app.js
+++ RESTful_BlogApp/app.js
@@ -1,4 +1,4 @@
-const   PORT            = process.env.PORT || 3000,
+const   PORT            = process.env.PORT || 5000,
         express         = require("express"), 
         app             = express(),
         bodyParser      = require("body-parser"),
