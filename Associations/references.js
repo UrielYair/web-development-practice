8321acd63068dccbcf7b82c11948f92b693d420d
Uriel Yair <uriel91@gmail.com>
2020-04-19 21:25:41 +0300

    Export post and user schema to 'models' folder.

--- Associations/references.js
+++ Associations/references.js
@@ -4,25 +4,8 @@ mongoose.connect('mongodb://localhost/blog_demo_2', {
         useUnifiedTopology: true
 });
 
-// Post - title, content
-const postSchema =  new mongoose.Schema({ 
-    title: String,
-    content: String 
-});
-const Post = mongoose.model('Post', postSchema);
-
-// User - email, name
-const userSchema =  new mongoose.Schema({ 
-    email: String,
-    name: String,
-    posts: [
-        {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-        }
-    ] 
-});
-const User = mongoose.model('User', userSchema);
+const Post = require("./models/post");
+const User = require("./models/user");
 
 // User.create({
 //     email: "bob@xyz.com",
@@ -30,8 +13,8 @@ const User = mongoose.model('User', userSchema);
 // });
 
 // Post.create({
-//     title: "How to cook the best burger? Pt. 2",
-//     content: "XYZ"
+//     title: "Hi!",
+//     content: "Bye!"
 // },(err,post)=>{
 //     if(err){
 //         console.log(err);
